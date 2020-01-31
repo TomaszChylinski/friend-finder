df9087100a198f1d9c7349c990d36b3f63e962bf
chylinski.t <chylinski.t@outlook.com>
2020-01-30 23:42:57 -0500

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friendsData = [
+  {
+    name: "Tom Hanks",
+    photo:
+      "https://cf-images.us-east-1.prod.boltdns.net/v1/static/769341148/75a00644-2472-4c82-b875-0920ffdbb32a/9ea183a8-d5f5-4787-a497-8e9e631578ec/1280x720/match/image.jpg",
+    scores: [4, 3, 2, 3, 4, 4, 3, 4, 3]
+  },
+  {
+    name: "Dwayne Johnson",
+    photo:
+      "https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_.jpg",
+    scores: [3, 4, 3, 4, 4, 1, 4, 5, 1]
+  },
+  {
+    name: "Kim Kardashian",
+    photo:
+      "https://www.etonline.com/sites/default/files/styles/video_1920x1080/public/images/2019-12/eto_dn_kimkardashian_121319_eh_jc_vidpic.jpg?itok=6kwb97Ia",
+    scores: [3, 5, 5, 5, 2, 1, 5, 4, 3]
+  },
+  {
+    name: "David Moyes",
+    photo:
+      "https://sm.imgix.net/18/34/david-moyes.jpg?w=640&h=480&auto=compress,format&fit=clip",
+    scores: [4, 4, 3, 3, 3, 5, 4, 2, 3]
+  },
+  {
+    name: "Priyanka Chopra",
+    photo:
+      "https://image.insider.com/5df4f1a479d75745a66269c2?width=1100&format=jpeg&auto=webp",
+    scores: [4, 1, 4, 5, 1, 1, 5, 5, 1]
+  }
+];
+
+// export
+module.exports = friendsData;
